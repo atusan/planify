751390d6e7225d577158da0cbb4d37a8a7854f2c
atusan <atunaseri@gmail.com>
2021-05-02 18:58:58 -0700

    datepickers in update page added

--- src/components/RemoveButton/RemoveButton.jsx
+++ src/components/RemoveButton/RemoveButton.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import { Button, Modal, Icon } from 'semantic-ui-react';
+import React from "react";
+import { Button, Modal, Icon } from "semantic-ui-react";
 
-export default function RemoveButton({ handleDeleteNote,note}) {
-    const [open, setOpen] = React.useState(false)
+export default function RemoveButton({ handleDeleteNote, note }) {
+  const [open, setOpen] = React.useState(false);
 
-    return (
-        <>
-        <Modal
-            closeIcon
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            size='mini'
-            trigger={<Button icon> <Icon name="delete" /></Button>}
-        >
-            <Modal.Content>
-                <p>Are you sure you want to delete this?</p>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button basic color='red' onClick={() => setOpen(false)}>
-                    <Icon name='remove' /> No
-                </Button>
-                <Button color='green'  onClick={() => {setOpen(false); handleDeleteNote(note._id);}} >
-                
-                    <Icon name='checkmark' /> Yes
-                </Button>
-            </Modal.Actions>
-        </Modal>
-        </>
-    )    
+  return (
+    <>
+      <Modal
+        closeIcon
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        size="small"
+        trigger={
+          <Button icon>
+            <Icon name="delete" />
+          </Button>
+        }
+      >
+        <Modal.Content>
+          <p>Are you sure you want to delete this?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="brown" inverted onClick={() => setOpen(false)}>
+            <Icon name="remove" /> No
+          </Button>
+          <Button
+            color="brown"
+            onClick={() => {
+              setOpen(false);
+              handleDeleteNote(note._id);
+            }}
+          >
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </>
+  );
 }
--- src/pages/EditNotePage/EditNotePage.jsx
+++ src/pages/EditNotePage/EditNotePage.jsx
@@ -1,13 +1,24 @@
 import React,{useState} from 'react';
 import {useLocation} from 'react-router-dom';
+import "../../public/styles/styles.css";
+import SemanticDatepicker from "react-semantic-ui-datepickers";
+import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
 import {Button, Form, Grid, Header, Image,  Segment } from 'semantic-ui-react';
 import PageHeader from '../../components/Header/Header';
 
 export default function EditNotePage(props){
     const location = useLocation();
     const data = location.state.note;
-    console.log(data);
+    console.log(data,'this is data coming');
     const [state, setState] = useState(data);
+
+    const [date, setDate] = useState(null);
+
+    let dateString = state.date;
+    const date1 = new Date(dateString);
+    // dateString = new Date(dateString).toUTCString();
+    // dateString = dateString.split(' ').slice(0, 4).join(' ');
+    console.log(date1,'this is state.date');
     
 
     function handleChange(e){
@@ -17,18 +28,39 @@ export default function EditNotePage(props){
         })
       }
       
-      function handleSubmit(e){
-          console.log(state,'handle submit to send new state')
-          e.preventDefault()
-          console.log('handleSubmit fired')
-          props.handleUpdateNote(state);
+    function handleDateChange(event, data) {
+      console.log(data.value, 'this is data.value')
+      let dateString = data.value;
+      // dateString = new Date(dateString).toUTCString();
+      // dateString = dateString.split(' ').slice(0, 4).join(' ');
+      // console.log(dateString);
+      setDate(dateString);
+    }
 
+      // function handleSubmit(e){
+      //     console.log(state,'handle submit to send new state')
+      //     e.preventDefault()
+      //     console.log('handleSubmit fired')
+      //     props.handleUpdateNote(state);
+
+      // }
+
+      function handleSubmit(e) {
+   
+        e.preventDefault();
+        const payload = {
+          ...state,
+          date,
+        };
+        props.handleUpdateNote(payload);
       }
 
     
     return(
-        <Grid textAlign='center' style={{ height: '25vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
+      <div className="bg-wall">
+        
+        <Grid textAlign='center' style={{ height: '75vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 650 }}>
         <Segment>
             <Form  autoComplete="off" onSubmit={handleSubmit}>
               <Form.Input
@@ -39,13 +71,14 @@ export default function EditNotePage(props){
                   onChange={handleChange}
                   required
               />   
-              <Form.Input
+              {/* <Form.Input
                   className="form-control"
                   name="date"
                   value={state.date}
                   placeholder="When"
                   onChange={handleChange} 
-              />   
+              />    */}
+               <SemanticDatepicker onChange={handleDateChange} value={date1}/>
               <Form.Input
                   className="form-control"
                   name="location"
@@ -70,5 +103,6 @@ export default function EditNotePage(props){
           </Segment>
       </Grid.Column>
     </Grid>
+    </div>
   ); 
 }
